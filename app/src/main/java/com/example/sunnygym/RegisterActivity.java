e8d9d38ca92853188ed03e933624173e628d189a
pauhernan7 <pauhernandez0204@gmail.com>
2025-03-18 18:27:58 +0100

    no se

--- app/src/main/java/com/example/sunnygym/RegisterActivity.java
+++ app/src/main/java/com/example/sunnygym/RegisterActivity.java
@@ -15,11 +15,20 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         Button registerConfirmButton = findViewById(R.id.registerConfirmButton);
+        Button backButton = findViewById(R.id.backButton);
 
         registerConfirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(RegisterActivity.this, RegistrationSuccessActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
